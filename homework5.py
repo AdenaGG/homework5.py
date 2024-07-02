c986f8dc5c55d89236e8698548c145853efe3f3d
Кахбек Халилов <profjeriko@outlook.com>
2024-07-02 18:28:14 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,7 @@
+immutable_var = (1, 's', [4, 5, 6.5], ['Gold'])
+print(immutable_var)
+#immutable_var [0] = 3
+#print(immutable_var) # кортеж нельзя редактировать или изменять
+mutable_list = [1], 3, 7, 'Serebro', ['W']
+mutable_list [4][0] = 'Forever'
+print(mutable_list)
